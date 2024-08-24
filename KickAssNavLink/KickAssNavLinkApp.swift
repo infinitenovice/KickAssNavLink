f0201d0470b1044fd35fe272404f4d4bfb023fd2
DRSaz <schmidliaz@gmail.com>
2024-08-24 16:35:05 -0700

    Initial Commit

---
+++ KickAssNavLink/KickAssNavLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickAssNavLinkApp.swift
+//  KickAssNavLink
+//
+//  Created by Infinite Novice on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct KickAssNavLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
